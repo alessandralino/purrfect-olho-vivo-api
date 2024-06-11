c558a7749c4c59f5c919aa037fd15723ef0a7fc4
alessandralino <ale.lino.00@gmail.com>
2024-06-11 19:20:31 +0100

    Endpoint de atualizar e ajuste de getAll

--- backend/Controllers/VeiculoController.cs
+++ backend/Controllers/VeiculoController.cs
@@ -20,10 +20,25 @@ namespace purrfect_olho_vivo_api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Veiculo>>> GetAll()
+        public async Task<ActionResult<IEnumerable<VeiculoGetAllResponse>>> GetAll()
         {
-            return await _context.Veiculo.ToListAsync();
+            var lista = await _context.Veiculo.Include(v => v.Linha).ToListAsync();
+
+            var responseList = lista.Select(v => new VeiculoGetAllResponse
+            {
+                Id = v.Id,
+                Name = v.Name,
+                Modelo = v.Modelo,
+                Linha = new Linha
+                {
+                    Id = v.Linha.Id,
+                    Name = v.Linha.Name,
+                }
+            }).ToList();
+
+            return Ok(responseList);
         }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Veiculo>> GetVeiculoById(int id)
@@ -96,5 +111,62 @@ namespace purrfect_olho_vivo_api.Controllers
             return NoContent();
         }
 
-    }        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<VeiculoUpdateResponse>> Update(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(veiculo).State = EntityState.Modified;
+
+            var veiculoResponse = new VeiculoUpdateResponse();
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                var veiculoWithLinha = await _context.Veiculo
+           .Include(v => v.Linha)
+           .FirstOrDefaultAsync(v => v.Id == veiculo.Id);
+
+                if (veiculoWithLinha == null)
+                {
+                    return NotFound();
+                }
+
+                veiculoResponse = new VeiculoUpdateResponse
+                {
+                    Id = veiculoWithLinha.Id,
+                    Name = veiculoWithLinha.Name,
+                    Modelo = veiculoWithLinha.Modelo,
+                    Linha = new Linha
+                    {
+                        Id = veiculoWithLinha.Linha.Id,
+                        Name = veiculoWithLinha.Linha.Name,
+                    }
+                };
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VeiculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction(nameof(GetVeiculoById), new { id = veiculo.Id }, veiculoResponse);
+        }
+
+
+        private bool VeiculoExists(int id)
+        {
+            return _context.Veiculo.Any(e => e.Id == id);
+        }
+    }
 }
---
+++ backend/ViewModels/Responses/VeiculoGetAllResponse.cs
@@ -0,0 +1,16 @@
+﻿using purrfect_olho_vivo_api.ViewModels.Models;
+
+namespace purrfect_olho_vivo_api.ViewModels.Responses
+{
+    public class VeiculoGetAllResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Modelo { get; set; }
+
+        public Linha Linha { get; set; }
+
+    }
+}
