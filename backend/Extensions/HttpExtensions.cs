9d552ee11f35bab4de16a0c69275c13b27bed9b0
alessandralino <ale.lino.00@gmail.com>
2024-11-21 15:02:08 +0000

    test: add unit test to get all paradas

--- backend/Extensions/HttpExtensions.cs
+++ backend/Extensions/HttpExtensions.cs
@@ -9,8 +9,15 @@ namespace purrfect_olho_vivo_api.Extensions
         public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
         {
             var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-            response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
-            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+            if(response != null)
+            {
+                if (response.Headers != null)
+                {
+                    response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+                    response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+                }
+            }
+            
         }
     }
 }
